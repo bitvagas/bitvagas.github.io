be6f274a64fa5929309c35e98ffc2104a8206a0f
Anderson Juhasc <anderson.juhasc@gmail.com>
2014-11-15 11:04:39 -0200

    init harpjs, gulp

---
+++ README.md
@@ -0,0 +1,15 @@
+# Harp, Gulp And BrowserSync
+
+A ready-to-go Gulp and Harp set up that does auto css injection via BrowserSync.
+
+## Install
+
+```console
+sudo npm install
+```
+
+run:
+
+```console
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var harp        = require('harp');
+
+/**
+ * Serve the Harp Site from the src directory
+ */
+gulp.task('serve', function () {
+  harp.server('.', {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: "localhost:9000",
+      open: false,
+      /* Hide the notification. It gets annoying */
+      notify: {
+        styles: ['opacity: 0', 'position: absolute']
+      }
+    });
+    /**
+     * Watch for changes
+     */
+    gulp.watch("**/*.{styl,sass,scss,less}", function () {
+      reload("main.css", {stream: true});
+    });
+
+    gulp.watch("**/*.{jade,haml,json}", reload)
+  })
+});
+
+/**
+ * Default task, running `gulp` will fire up the Harp site,
+ * launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve']);
