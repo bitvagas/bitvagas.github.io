a90350acb4ec0dbd7a25d64481e0589dd2625249
Randson Oliveira <randsonjs@gmail.com>
2014-11-18 10:38:34 -0400

    Update script for deploy

--- compile.sh
+++ compile.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+SOURCE_BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+if [ $# -ge 1 ]; then
+  DEST_BRANCH=$1
+else
+  DEST_BRANCH=master
+fi
+
 harp compile
 
-## Deploy to master
-git commit -am "Deploying to master..."
-git checkout master
-mv www/* ./
-rm -r www/
+if [ "$SOURCE_BRANCH" = "$DEST_BRANCH" ]; then 
+  echo "You can't deploy to $DEST_BRANCH from $SOURCE_BRANCH"
+  exit 1
+fi
 
-echo "\e[1;32mDeploy successful!"
+git checkout $DEST_BRANCH
+cp www/* . && rm -r www/
+git add .
+git commit -m "Deploying to $DEST_BRANCH"
+git push origin $DEST_BRANCH
