cf25060950c7722e226f4612889eb9ab89b63473
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-01-10 00:28:26 -0200

    add js file

---
+++ public/js/bitvagas.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#qrcode').qrcode({
+        "text": "bitcoin:1MckjA4yZWgwo4kXFnE6ikbfPPuB3yCcx3?label=BitVagas&message=Donation%20for%20Bitvagas",
+        "width": 180,
+        "height": 180
+    });
+});
